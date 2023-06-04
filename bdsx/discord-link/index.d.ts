d23d2027b9ae1f50528fca7e5ceee64f3818550c
DerCoderJo <108522996+DerCoderJo@users.noreply.github.com>
2023-06-04 18:42:57 +0200

    Delete bdsx directory

--- bdsx/discord-link/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function getUserByXuid(xuid: String): any;
-export declare function getPlayerById(id: String): any;
-export declare function addRoleToUser(userId: String, roleId: String): boolean;
-export declare function removeRoleFromUser(userId: String, roleId: String): boolean;
